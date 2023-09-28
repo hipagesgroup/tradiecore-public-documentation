637746a370672692127a37c3b482ccf48d86b505
Mela-C <melanieching@hipagesgroup.com.au>
2023-09-28 17:12:04 +1000

    Committing file:///C:/Tradiecore - Publishing/Data/SearchPhrase_Chunk3.js file:///C:/Tradiecore - Publishing/Data/SearchPhrase_Chunk2.js file:///C:/Tradiecore - Publishing/Resources/Scripts/require.config.js file:///C:/Tradiecore - Publishing/Data/SearchTopic_Chunk0.js file:///C:/Tradiecore - Publishing/Data/SearchStem_Chunk0.js file:///C:/Tradiecore - Publishing/Data/SearchPhrase_Chunk1.js file:///C:/Tradiecore - Publishing/Data/SearchPhrase_Chunk0.js file:///C:/Tradiecore - Publishing/Data/Search.js file:///C:/Tradiecore - Publishing/Data/HelpSystem.xml file:///C:/Tradiecore - Publishing/Data/HelpSystem.js file:///C:/Tradiecore - Publishing/Content/A_Introduction Topics/Getting Started.htm file:///C:/Tradiecore - Publishing/Default.mcwebhelp file:///C:/Tradiecore - Publishing/HTML5 - Top Navigation.4.log.zip

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['1'],p:[['1',29],['from',42],['require',11],['your',49]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['1'],p:[['1',29],['from',42],['required',27],['your',53]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638315171346074358'
+    urlArgs: 't=638315179205067265'
 });
